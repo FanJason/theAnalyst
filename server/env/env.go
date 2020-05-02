1c46fbd500f3fb752e9558e05fd462e16bb05bb7
Jason Fan <jf7776@gmail.com>
2020-05-01 17:08:55 -0700

    init react files

--- server/env/env.go
+++ server/env/env.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetEnvVariable(key string) string {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		fmt.Printf("failed to get env variable: %v", err)
 	}
